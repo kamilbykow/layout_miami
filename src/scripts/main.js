e39b20da1b6ad10a6480906b553014e2f752c904
Kamil Bykowski <kamilbykowski99@gmail.com>
2025-11-18 14:21:27 +0100

    Apply checklist guidelines

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+})
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
